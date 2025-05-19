895a4a7cea8855bc1ef889541c0f0b91bb3e7936
sirWerq <rifqx1904@gmail.com>
2025-05-19 12:04:47 +0700

    Add CORS support to the application and update package dependencies

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 const authRouter = require("./routes/authRoutes");
 
+app.use(
+    cors({
+        origin: "http://localhost:3001",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 app.use(authRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
